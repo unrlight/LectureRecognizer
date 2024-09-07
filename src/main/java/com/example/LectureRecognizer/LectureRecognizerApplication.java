26b146fe80fd28e02965968b48b51407bf616f5e
unrlight <unrlight@gmail.com>
2024-09-08 00:19:11 +0500

    initial commit

---
+++ src/main/java/com/example/LectureRecognizer/LectureRecognizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.LectureRecognizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureRecognizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureRecognizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LectureRecognizer/LectureRecognizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LectureRecognizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureRecognizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
